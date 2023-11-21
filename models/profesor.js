253959c06b43de4d6a73574c0351f625a42216b2
omartinez1204 <omar1204@gmail.com>
2023-11-21 13:18:44 -0600

    Todos los cambios hasta ahora

---
+++ models/profesor.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const db = require('../database/db')
+const Schema = mongoose.Schema;
+
+const ProfesorSchema = new Schema({
+    
+})
+
+module.exports = ProfesorSchema
