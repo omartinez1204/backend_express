fd021773ee7f4fb9526f4583632c5a08659eb284
omartinez1204 <omar1204@gmail.com>
2023-11-21 13:25:33 -0600

    add a model

--- models/profesor.js
+++ models/profesor.js
@@ -3,7 +3,17 @@ const db = require('../database/db')
 const Schema = mongoose.Schema;
 
 const ProfesorSchema = new Schema({
-    
+    nombre: {
+        type: String,
+        required: true,
+    },
+    apellidos:{
+        type: String,
+        required: true,
+    },
+    carrera,
+    materias,
+    linea_investigacion,
 })
 
 module.exports = ProfesorSchema
